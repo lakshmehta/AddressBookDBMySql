6f8cadf4a9900f05a8b337068b9eb758e03253db
unknown <lakshmehta06@gmail.com>
2021-04-28 14:06:07 +0530

    [Laksh added Insert data to a table]

--- CreateDB.sql
+++ CreateDB.sql
@@ -11,3 +11,15 @@ create table AddressBook
     zip int,
     phone_number varchar(10) not null
 );
+
+
+--[UC3]
+
+insert into AddressBook values
+('Laksh','Mehta','Ramnagar.','Khandwa','MP',450001,'8966049333'),
+('Sumit','Patidar','Anand Nagar.','Khandwa','MP',450001,'9876563210'),
+('Naman','Mathur','puliya.','Indore','MP',400001,'8976543210'),
+('Aman','Rawat','jhanda choak.','Bhopal','MP',462024,'8755412345'),
+('Saurabh','Sajwan',' ghantagher.','Kareli','MP',895761,'7856235620'),
+('Gaurabh','Butola','kotewser.','Dehli','Delhi',895154,'9875141545');
+select * from AddressBook
